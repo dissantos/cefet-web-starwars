2bf064e86bce90dd5449f758ea468b8ee9c571b0
dissantos <disantosg18@gmail.com>
2021-12-22 14:56:02 -0300

    - Add music player

--- starwars.js
+++ starwars.js
@@ -4,4 +4,22 @@
 //  - Pegar a lista de filmes (AJAX) e preencher no HTML
 //  - Quando um filme for clicado, exibir sua introdução
 
+import { play } from "./music.js";
 const API_ENDPOINT = 'https://swapi.dev/api'
+
+
+//Exercicio 1
+class Audio {
+    constructor(audioUrl, coverImageUrl, title, artist){
+        this.audioUrl = audioUrl;
+        this.coverImageUrl = coverImageUrl;
+        this.title = title;
+        this.artist = artist;
+    }
+}
+
+let audioSWTheme = new Audio('audio/tema-sw.mp3','imgs/logo.svg','Intro', 'John Willians');
+let bodyEl = document.body;
+
+play(audioSWTheme, bodyEl);
+
