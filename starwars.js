9310c4be6e3202a90455f285d564117f6b4f36c7
dissantos <disantosg18@gmail.com>
2021-12-22 17:13:24 -0300

    - Add movie list and set intro

--- starwars.js
+++ starwars.js
@@ -5,6 +5,7 @@
 //  - Quando um filme for clicado, exibir sua introdução
 
 import { play } from "./music.js";
+import { restartAnimation } from "./restart-animation.js";
 const API_ENDPOINT = 'https://swapi.dev/api'
 
 
@@ -23,3 +24,52 @@ let bodyEl = document.body;
 
 play(audioSWTheme, bodyEl);
 
+//Exercicio 2 e 3, Opcional 4
+const response = await fetch(API_ENDPOINT+'/films')
+const data = await response.json();
+
+
+let movieList = data.results.sort((a, b) => a.episode_id - b.episode_id);
+
+
+const numberToRoman = (number) => {
+    const romans = {
+        1 : 'I',
+        2 : 'II',
+        3 : 'III',
+        4 : 'IV',
+        5 : 'V',
+        6 : 'VI',
+        7 : 'VII',
+        8 : 'VIII',
+        9 : 'IX'
+    };
+
+    return romans[number];
+}
+
+
+let listEl = document.querySelector('#filmes ul');
+listEl.innerHTML = '';
+
+for( let movie of movieList ) {
+    let roman = numberToRoman(movie.episode_id).padEnd(4, ' ');
+    let title = movie.title;
+    let domFragment = document.createDocumentFragment();
+    let liEl = document.createElement('li');
+    liEl.textContent = `Episode ${roman}-  ${title}`
+    
+    liEl.addEventListener('click', () => {
+        let introEl = document.querySelector('.introducao-animada');
+        
+        introEl.innerHTML = `
+            Episode ${roman}
+            ${title.toUpperCase()}
+
+            ${movie.opening_crawl}
+
+        `
+        restartAnimation(introEl);
+    })
+    listEl.append(liEl);
+}
